50d845cdbec846cd1f50cddc4b37b909541fae71
Hcordes6 <hcordesmn@gmail.com>
2025-10-09 18:30:16 -0500

    Added app development coming soon in app tab

--- script.js
+++ script.js
@@ -1,34 +1,41 @@
 
 
 
-const allButton = document.querySelector(".all-button");
-const webButton = document.querySelector(".web-button");
-const mobileButton = document.querySelector(".mobile-button");
-const miscButton = document.querySelector(".misc-button");
-const webProjects = document.querySelectorAll(".web-projects");
-const mobileProjects = document.querySelectorAll(".mobile-projects");
-const miscProjects = document.querySelectorAll(".misc-projects");
+const allButton = document.querySelector(".all-button")
+const webButton = document.querySelector(".web-button")
+const mobileButton = document.querySelector(".mobile-button")
+const miscButton = document.querySelector(".misc-button")
+const webProjects = document.querySelectorAll(".web-projects")
+const mobileProjects = document.querySelectorAll(".mobile-projects")
+const miscProjects = document.querySelectorAll(".misc-projects")
 
+const comingSoon = document.querySelectorAll(".coming-soon")
 
 
 allButton.addEventListener('click', () => {
-    webProjects.forEach(project => project.style.display = 'inline-block') // Help from w3schools
+    webProjects.forEach(project => project.style.display = 'inline-block') // forEach help from w3schools
     mobileProjects.forEach(project => project.style.display = 'inline-block')
-    miscProjects.forEach(project => project.style.display = 'inline-block') 
+    miscProjects.forEach(project => project.style.display = 'inline-block')
+    comingSoon.forEach(project => project.style.display = 'none')
+
 })
 webButton.addEventListener('click', () => {
-    webProjects.forEach(project => project.style.display = 'inline-block') 
-    mobileProjects.forEach(project => project.style.display = 'none') 
-    miscProjects.forEach(project => project.style.display = 'none') 
+    webProjects.forEach(project => project.style.display = 'inline-block')
+    mobileProjects.forEach(project => project.style.display = 'none')
+    miscProjects.forEach(project => project.style.display = 'none')
+    comingSoon.forEach(project => project.style.display = 'none')
 })
 mobileButton.addEventListener('click', () => {
-    webProjects.forEach(project => project.style.display = 'none') 
-    mobileProjects.forEach(project => project.style.display = 'inline-block') 
-    miscProjects.forEach(project => project.style.display = 'none') 
+    webProjects.forEach(project => project.style.display = 'none')
+    mobileProjects.forEach(project => project.style.display = 'inline-block')
+    miscProjects.forEach(project => project.style.display = 'none')
+    comingSoon.forEach(project => project.style.display = 'inline-block')
 })
 miscButton.addEventListener('click', () => {
-    webProjects.forEach(project => project.style.display = 'none') 
-    mobileProjects.forEach(project => project.style.display = 'none') 
-    miscProjects.forEach(project => project.style.display = 'inline-block') 
+    webProjects.forEach(project => project.style.display = 'none')
+    mobileProjects.forEach(project => project.style.display = 'none')
+    miscProjects.forEach(project => project.style.display = 'inline-block')
+    comingSoon.forEach(project => project.style.display = 'none')
+
 })
 
